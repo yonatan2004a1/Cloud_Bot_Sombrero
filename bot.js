84082e70bbe1168577e95364f7c0b7009ca3e550
jonathan <prozeril12@gmail.com>
2020-04-02 18:16:17 +0300

    first commit

---
+++ bot.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log("the bot is online")
+})
+
+
+bot.login('Njk0Mjk3NzA4MDM0MzkyMDk1.XoOR4w.RtbhFkDaKzmta4MNmBWBLndhGuE');
