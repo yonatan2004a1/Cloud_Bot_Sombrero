13745aff4930855f1785d2f017f779de55fc1919
jonathan <MY_NAME@example.com>
2020-04-06 14:31:03 +0300

    survival GAME testing

--- bot.js
+++ bot.js
@@ -13,7 +13,6 @@ bot.on('message', (message) => {
     var msg = message.content.toUpperCase(); // Take the messsage and make it uppercase.
     var number = message.content; // Numbers that sent at counting to 10k.
     //let args = message.content.substring(prefix.lenght).split(" ")
-
     
     if (msg.includes('LAYLA') || msg.includes('לילה')) {
         message.channel.send('Layli lay.');
@@ -27,7 +26,7 @@ bot.on('message', (message) => {
         message.channel.send('FoX1E is my lord.');
     }
 
-//================================================================================================================================================================================================
+//counter_count chat
   
     if (message.channel.id === '612392493987921930') // counting to 10k chat.
     {
@@ -40,7 +39,7 @@ bot.on('message', (message) => {
         }
 
         if (numberCheck == true) {
-            if (number - 1 != 4495 + counter) // Checks if the number is incorrect or duplicate.
+            if (number - 1 != 4501 + counter) // Checks if the number is incorrect or duplicate.
             {
                 message.delete();
                 message.author.send('>>> You have entered an **incorrect** or **duplicate** number, \nPlease re-enter a **correct** number at <#612392493987921930>.');
@@ -61,7 +60,7 @@ bot.on('ready', () => {
     console.log("[BOT] Logged in as " + bot.user.tag);
 })
 
-//================================================================================================================================================================================================
+//welcome + add role for the new users
 
 bot.on('guildMemberAdd', member => {
     // Add role for the new member
@@ -151,6 +150,11 @@ bot.on('raw', event => {
 });
 
 bot.on('messageReactionAdd', (messageReaction, user) => {
+    if(messageReaction.emoji.name === '✔️' || messageReaction.emoji.name === '✖️')
+    {
+        return
+    }
+    
     var roleName = messageReaction.emoji.name;
     var role = messageReaction.message.guild.roles.find(role => role.name.toLowerCase() === roleName.toLowerCase());
 
@@ -162,9 +166,15 @@ bot.on('messageReactionAdd', (messageReaction, user) => {
             member.addRole(role.id);
         }
     }
+    
 });
 
 bot.on('messageReactionRemove', (messageReaction, user) => {
+    if(messageReaction.emoji.name === '✔️' && messageReaction.emoji.name === '✖️')
+    {
+        return
+    }
+
     var roleName = messageReaction.emoji.name;
     var role = messageReaction.message.guild.roles.find(role => role.name.toLowerCase() === roleName.toLowerCase());
 
@@ -176,54 +186,155 @@ bot.on('messageReactionRemove', (messageReaction, user) => {
             member.removeRole(role.id);
         }
     }
+
 });
 
 
 
 
-//Survival GAME-comming soon
-/*bot.on('message', (message)=> {
-    let args2 = message.content.substring(PREFIX.length).split(" ")//arry-commends
-    var sender = message.author;//the guy who started the game
+//Survival GAME
+const userCreatedPolls = new Map();
 
-    switch(args2[0])
+bot.on('message', async message =>{
+    if(message.author.bot)
     {
-        case 'survival':
-            
-            const SurvivalGame = require('./addRoles')
-            message.channel.send('How many player participate in the SURVIVAL?');
-
-        break;
-
-        case 'players':
-            var SnumberCheck = true;
-            if(isNaN(args2[1]))
+        return;
+    }
+    if(message.content.toLowerCase() === '!survival')
+    {
+        if (message.channel.id === '696409036790431785')
+        {
+            if(userCreatedPolls.has(message.author.id))
             {
-                message.author.send("pls enter numbers :) ");
-                SnumberCheck = false;
+                message.channel.send("You already have a survival game going on. pls stop doing that")
                 return;
             }
-            if(SnumberCheck==true)
-            {
-                for(var i=1;i<=args2[1];i++)
+            message.channel.send("yo btch pls enter the players pls :)");
+            let filter = m => {
+                if(m.author.id === message.author.id)
                 {
-                    message.channel.send(i + " name: ");
-
-                        if(sender =! message.author)
-                        {
-                            return;
-                        }
-                        else
-                        {
-                            bot.on('message', (Smsg) => { 
-                            Smsg.react("🏴‍☠️");
-                            })
-                        }
-    
+                    if(m.content.toLowerCase() === 'done')
+                    {
+                        collector.stop();
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
                 }
             }
-        break;
+            let collector = message.channel.createMessageCollector(filter, {maxMatches:10} );
+            let pollOptions = await getPollOptions(collector);
+            if(pollOptions.length < 2)
+            {
+                message.channel.send("dont play this game alone you cunt!");
+                return;
+            }
+            let embed = new Discord.MessageEmbed();
+            embed.setTitle("Your list")
+            embed.setDescription(pollOptions.join("\n"));
+            let confirm = await message.channel.send(embed);
+            await confirm.react("✔️");
+            await confirm.react("✖️");
+        
+            let reactionFilter = (reaction, user) => (user.id === message.author.id) && !user.bot;
+            let reaction = (await confirm.awaitReactions(reactionFilter, {max :1})).first();
+            if(reaction.emoji.name === '✔️')
+            {
+                message.channel.send("THE game will begin in 5 seconds so be ready");
+                await delay(5000);
+                message.channel.send("VOTE NOW!");
+                let userVotes = new Map();
+                let pollTally = new Discord.Collection(pollOptions.map(o =>[o, 0]))// arry inside arry (double arry)
+                let pollFilter = m => !m.bot;
+                let voteCollector = message.channel.createMessageCollector(pollFilter, {
+                time: 60000//the time for voting
+                });
+                userCreatedPolls.set(message.author.id, voteCollector)
+                await processPollResults(voteCollector, pollOptions, userVotes, pollTally);
+                let max = Math.max(...pollTally.array());// the dots is for printing the arry without them []
+                let entries = [...pollTally.entries()];
+                let winners = [];
+                let embed = new Discord.MessageEmbed();
+                let desc = '';//description
+                entries.forEach(entry => entry[1] === max ? winners.push(entry[0]) : null);
+                entries.forEach(entry => desc += entry[0]+ " received " + entry[1] + " votes\n");
+                embed.setDescription(desc);
+
+                if(winners.length === 1)
+                {
+                    message.channel.send(winners[0] + " is the ONE that will play alone ", embed);
+                }
+                else
+                {
+                    message.channel.send("We have a draw", embed);
+
+                }
+        }
+        else if(reaction.emoji.name === '❌')
+        {   
+            message.channel.send("SURVIVAL cancelled");
+        }
+
+        }
+        else if(message.content.toLowerCase() === '!stopvote')
+        {
+            if(userCreatedPolls.has(message.author.id))
+            {
+                console.log("Trying to stop poll.");
+                userCreatedPolls.get(message.author.id).stop();
+                userCreatedPolls.delete(message.author.id);
+
+            }
+            else
+            {
+                message.channel.send("You dont have a 'survival' goin on :(");
+            }
+        }
     }
-    
-     
-})*/
+
+});
+
+function processPollResults(voteCollector, pollOptions, userVotes, pollTally)
+{
+    return new Promise((resolve, reject) =>{
+        voteCollector.on('collect', msg =>{
+            let option = msg.content.toLowerCase();
+            if(!userVotes.has(msg.author.id) && pollOptions.includes(option))
+            {
+                userVotes.set(msg.author.id, msg.content);
+                let voteCount = pollTally.get(option);
+                pollTally.set(option, ++voteCount);
+            }
+        });
+        voteCollector.on('end', collected => {
+            console.log("Collected" + collected.size + "vote.");
+            resolve(collected);
+        })
+    });
+}
+
+
+
+
+function getPollOptions(collector)
+{
+    return new Promise((resolve, reject) => {
+        collector.on('end', collected => resolve(collected.map(m => m.content.toLowerCase())))
+    });
+
+}
+
+function delay(time)
+{
+    return new Promise((resolve, reject) =>{
+        setTimeout(() => {
+            resolve();
+        }, time)
+    })
+}
+
