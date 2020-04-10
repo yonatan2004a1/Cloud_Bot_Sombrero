9ef43adb6a94206566971f1238c7f3ba13070f01
Ofek Askayo <ofek0711@gmail.com>
2020-04-10 17:27:30 +0300

    Remove console deleted message log & Fix syntax

--- bot.js
+++ bot.js
@@ -64,15 +64,16 @@ bot.on('message', async (message) => {
             }
 
             const fetched = await message.channel.fetchMessages({limit: args[0]}); // This grabs the last number(args) of messages in the channel.
-            console.log(fetched.size + ' messages found, deleting...');
             var reason = args[1];
-            if(!reason)
+
+            if (!reason)
             {
-                message.channel.send('pls enter a reason to clear the messages :) \nUsage: \`' + PREFIX + 'clear ' + fetched.size +  ' <reason>\`')
+                message.channel.send('Please enter a reason to clear the messages. \nUsage: \`' + PREFIX + 'clear ' + fetched.size +  ' <reason>\`')
                 return;
             }
+
             var clearChannel = message.channel.name;
-            bot.channels.get(process.env.CLEARLOG_ACTIVE_CHAT_ID).send(message.author.toString() + ' Deleted: ' + fetched.size + ' messages. From: ' + clearChannel + ' chat. The reason is: ' + reason);
+            bot.channels.get(process.env.CLEARLOG_ACTIVE_CHAT_ID).send(message.author.toString() + ' Deleted: ' + fetched.size + ' messages. \nFrom: ' + clearChannel + 'text channel. \nReason: ' + reason + '.');
             console.log(clearChannel);
 
             // Deleting the messages
@@ -80,9 +81,6 @@ bot.on('message', async (message) => {
                 .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.
         }
         clear(); 
-       
-        
-
     }
 
 //counter_count chat
