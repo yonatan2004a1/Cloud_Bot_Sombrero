4677e782807630ee3590f502bd45900ff94f6f80
jonathan <yonatan2004s@gmail.com>
2020-05-02 01:44:46 +0300

    Pull info form lol API

--- lol.js
+++ lol.js
@@ -1,5 +1,5 @@
 //var RiotRequest = require('riot-lol-api');
-
+const fetch = require("node-fetch");
 const common = require('./common.js');
 const Discord = require('discord.js');
 const bot = common.bot;
@@ -8,7 +8,14 @@ const PREFIX = common.PREFIX;
 //const riotKey = new RiotRequest('RGAPI-760c5463-f8ba-471f-ad17-2a948758088a')
 const key = "RGAPI-760c5463-f8ba-471f-ad17-2a948758088a";
 const region = "EUN1";
-const url = region + ".api.riotgames.com" + "/lol/summoner/v4/summoners/by-name/loger2" + "?api_key=" + key;
-const response = fetch(url);
-const data = response.json();
+const url = "https://" + region + ".api.riotgames.com" + "/lol/summoner/v4/summoners/by-name/loger2" + "?api_key=" + key;
+const response = fetch(url)
+.then(response => response.json())
+.then(data => console.log(data));
+
+
+
+
+/*const data = response.json();
 console.log(data.id)
+console.log(url)*/
