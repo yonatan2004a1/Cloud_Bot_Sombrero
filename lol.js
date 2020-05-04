9a29d095fa9c2761f03e084d41aa82dc2704a6dd
jonathan <yonatan2004s@gmail.com>
2020-05-05 00:35:21 +0300

    Bug fix

--- lol.js
+++ lol.js
@@ -5,14 +5,19 @@ const Discord = require('discord.js');
 const bot = common.bot;
 const db = common.db;
 const PREFIX = common.PREFIX;
+
 //const riotKey = new RiotRequest('RGAPI-760c5463-f8ba-471f-ad17-2a948758088a')
 const key = "RGAPI-760c5463-f8ba-471f-ad17-2a948758088a";
-const region = "EUN1";
-const url = "https://" + region + ".api.riotgames.com" + "/lol/summoner/v4/summoners/by-name/loger2" + "?api_key=" + key;
-const response = fetch(url)
-.then(response => response.json())
-.then(data => console.log(data));
-
+function getID(summonerName, region)
+{
+    var url = "https://" + region + ".api.riotgames.com" + "/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + key;
+    var data = response.json();
+    fetch(url)
+    .then(response => response.json())
+    .then(data => data.id)
+    return data.id;
+}
+console.log(getID("loGer2", "eun1"))
 
 
 
