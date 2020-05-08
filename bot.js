0ceba0f6848f4474bc8205760abcbc3e4e1637dc
Ofek Askayo <ofek0711@gmail.com>
2020-05-08 13:29:05 +0300

    Add flex and solo/duo ranks into a rich embed

--- bot.js
+++ bot.js
@@ -117,9 +117,11 @@ bot.on('message', async (message) => {
             let region = args[args.length-1];
             leagueAPI.GetUsernameAndRank(name, region)
             .then(data => {
+                let ranks = data[1];
                 embed.setTitle(data[0] + "'s Stats");
                 embed.addField("Summoner name" , data[0]);
-                embed.addField("Rank" , data[1]);
+                embed.addField("Ranked Solo/Duo" , ranks[0].rank);
+                embed.addField("Ranked Flex" , ranks[1].rank); 
                 embed.setColor("#cf95f8");
                 embed.setFooter("The next baron bot???");
                 leagueAPI.GetProfileIconURL(data[0], region)
