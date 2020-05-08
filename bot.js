8c8a9bca1bf66cbc911fbfba25cc4fbce5cf28f9
Ofek Askayo <ofek0711@gmail.com>
2020-05-08 22:01:20 +0300

    Fix stytax

--- bot.js
+++ bot.js
@@ -129,7 +129,7 @@ bot.on('message', async (message) => {
                     user = message.author;
                 }
                 embed.setTitle(data[0] + "'s stats");
-                embed.addField("Level: " , data[2]);
+                embed.addField("Level" , data[2]);
                 embed.addField("Solo/Duo" , ranks[0].rank +'\n'+ranks[0].games +'\n'+ranks[0].winRate , true);
                 embed.addField("Flex 5v5" , ranks[1].rank +'\n'+ranks[1].games +'\n'+ranks[1].winRate , true); 
                 embed.setColor("#cf95f8");
