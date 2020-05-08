e76a83354eb9079f742406040aae4715b45af26e
Ofek Askayo <ofek0711@gmail.com>
2020-05-08 12:19:02 +0300

    Replace setImage to setThumbnail

--- bot.js
+++ bot.js
@@ -92,7 +92,7 @@ bot.on('message', async (message) => {
                     embedClear.addField("Reason: " , reason);
                     embedClear.setFooter("You can ban tiran if you want to do so :>");
                     embedClear.setColor("#fffefe");
-                    embedClear.setImage(user.avatarURL);
+                    embedClear.setThumbnail(user.avatarURL);
                     bot.channels.get(process.env.CLEARLOG_ACTIVE_CHAT_ID).send(embedClear);
                 })
                 .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.
