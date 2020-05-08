5f71b41dbca7a90f4075a249911d09a26e897276
Ofek Askayo <ofek0711@gmail.com>
2020-05-08 21:21:14 +0300

    Fix wrong syntax in lol stats

--- bot.js
+++ bot.js
@@ -109,7 +109,7 @@ bot.on('message', async (message) => {
         }
         if(!args[0] || !args[1])
         {
-            message.channel.send("The search command allows you to search a league account\nsyntax: \`" + PREFIX + 'stats <name> <region>\`');
+            message.channel.send("The stats command allows you to search a league account\nsyntax: \`" + PREFIX + 'stats <name> <region>\`');
         }
         else
         {
