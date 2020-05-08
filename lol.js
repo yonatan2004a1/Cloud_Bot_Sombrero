5334d432defc184765e410e78a66dc6b2a5b2651
jonathan <yonatan2004s@gmail.com>
2020-05-08 20:44:25 +0300

    Fixed the wr

--- lol.js
+++ lol.js
@@ -54,6 +54,7 @@ async function GetRankAndTier(id, region) //WILL ALWAYS RETURN SOLO DUO AS [0] A
         }
         let games = data[0].wins + data[0].losses;
         var winRate = (data[0].wins/games) * 100;
+        winRate.toFixed(0);
         let ranks = [];
         if(data.length == 1)
         {
