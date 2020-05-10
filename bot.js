e2335daa42f1db7614a83c8fe522f4bd13dec103
Ofek Askayo <ofek0711@gmail.com>
2020-05-10 23:24:10 +0300

    Improve command list command syntax

--- bot.js
+++ bot.js
@@ -160,9 +160,9 @@ bot.on('message', async (message) => {
         let embedCommandList = new Discord.RichEmbed();
 
         embedCommandList.setTitle("Sombrero Guy's Command List");
-        embedCommandList.addField("🧹 Clear" , "`*clear <amount> <reason>`\n(Only usable by the staff)" , true);
-        embedCommandList.addField("📊 Stats" , "`*stats <name> <region>`\n(Only usable in <#694548662919102534> & <#673956291219292179> text channel)" , true);
-        embedCommandList.addField("🌴 Survival" , "`*survival`\n(Only usable in <#696784664261689488> text channel)" , true);
+        embedCommandList.addField("🧹 Clear" , "`*clear <amount> <reason>`\n(Only usable by the staff)");
+        embedCommandList.addField("📊 Stats" , "`*stats <name> <region>`\n(Only usable in <#694548662919102534> & <#673956291219292179> text channel)");
+        embedCommandList.addField("🌴 Survival" , "`*survival`\n(Only usable in <#696784664261689488> text channel)");
         embedCommandList.setColor("#7289da");
         message.channel.send(embedCommandList);
     }
