1b336375b50c300439299c54e0af4fcb46619c7d
Ofek Askayo <ofek0711@gmail.com>
2020-05-21 16:35:12 +0300

    Change below 30 degrees color

--- bot.js
+++ bot.js
@@ -218,13 +218,13 @@ bot.on('message', async (message) => {
                 // embed.addField("Minimum temperature" , data[3] + "°");
                 // embed.addField("Maximum temperature" , data[4] + "°");
                 embed.addField("Wind speed" , data[5] + " km/h");
-                if(data[0] > 39)
+                if(data[0] > 30)
                 {
                     embed.setColor("#ffae30");
                 }
                 else
                 {
-                    embed.setColor("#30bfee");
+                    embed.setColor("#5caae6");
                 }
                 embed.setTimestamp();
                 embed.setFooter("Check out the current weather!", "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/248/sun-behind-rain-cloud_1f326.png");
