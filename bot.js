f1ae271f7cfdd155c6d2b2cdba78f906e6c21a63
jonathan <yonatan2004s@gmail.com>
2020-06-13 15:48:41 +0300

    add members counter

--- bot.js
+++ bot.js
@@ -368,7 +368,7 @@ bot.on('message', async (message) => {
     if(msg.startsWith(PREFIX + 'SERVER'))
     {
         let statsGuild = bot.guilds.get('566917751709499392');
-        let memberCount = statsGuild.memberCount;
+        let countMember = statsGuild.memberCount;
         let embedStats = new Discord.RichEmbed();
         embedStats.setAuthor("POCO_LOCO's Lounge 🤠" , 'https://cdn.discordapp.com/attachments/694702052831395890/721309287200063538/pocoloco.jpg');
         embedStats.addField(`👥 Members ` , memberCount + " Members. ")
