1a73a1bcd10830d00c5e7a0d954eeb9b3c43d902
Ofek Askayo <ofek0711@gmail.com>
2020-06-14 11:09:42 +0300

    Add config var of server ID, change the author from constants to variables

--- bot.js
+++ bot.js
@@ -367,14 +367,16 @@ bot.on('message', async (message) => {
     // Server Information Command
     if(msg.startsWith(PREFIX + 'SERVERINFO'))
     {
-        let guild = bot.guilds.get('697109578868981840');
+        let guild = bot.guilds.get(process.env.SERVER_ID);
+        let serverName = message.guild.name;
+        let serverIcon = message.guild.iconURL;
         let owner = message.guild.member(guild.owner) ? guild.owner.toString() : guild.owner.user.tag;
         let members = guild.members.filter(member => !member.user.bot).size; 
         let onlineMembers = guild.members.filter(m => m.presence.status === 'online').size;
         let bots = guild.members.filter(member => member.user.bot).size;
         
         let embedStats = new Discord.RichEmbed();
-        embedStats.setAuthor("POCO_LOCO's Lounge 🤠" , 'https://cdn.discordapp.com/attachments/694702052831395890/721309287200063538/pocoloco.jpg');
+        embedStats.setAuthor(serverName , serverIcon);
         embedStats.addField('👑 Owner', owner)
         embedStats.addField(`👥 Members (${members})` , `**Bots:** ${bots}\n**Online:** ${onlineMembers}`)
         message.channel.send(embedStats);
