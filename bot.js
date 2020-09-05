2c6c56dfba8a1b6befc2be27dcd4a6d05266f428
Ofek Askayo <ofek0711@gmail.com>
2020-09-06 00:32:23 +0300

    Fix game selection syntax

--- bot.js
+++ bot.js
@@ -73,7 +73,7 @@ bot.on('message', async (message) => {
         {
             gameSelectionEmbed.setTitle("You've reached the game selection channel!");
             gameSelectionEmbed.setDescription("Here you can associate yourself with the gaming communities you play in!\nPlease click on the emojis below the message that corresponding for the games you play.");
-            gameSelectionEmbed.addField("Games selection:" , `<:League_of_Legends:${process.env.LEAGUE_OF_LEGENDS_EMOJI_ID}> **:** League of Legends\n:<:VALORANT:${process.env.VALORANT_EMOJI_ID}> **:** VALORANT\n:<:Among_Us:${process.env.AMONG_US_EMOJI_ID}> **:** Among Us\n:<:Minecraft:${process.env.MINECRAFT_EMOJI_ID}> **:** Minecraft`);
+            gameSelectionEmbed.addField("Games selection:" , `<:League_of_Legends:${process.env.LEAGUE_OF_LEGENDS_EMOJI_ID}> **:** League of Legends\n<:VALORANT:${process.env.VALORANT_EMOJI_ID}> **:** VALORANT\n<:Among_Us:${process.env.AMONG_US_EMOJI_ID}> **:** Among Us\n<:Minecraft:${process.env.MINECRAFT_EMOJI_ID}> **:** Minecraft`);
             gameSelectionEmbed.setColor("fcb040");
             gameSelectionEmbed.setImage('https://cdn.discordapp.com/attachments/420122298805125120/751770348025806864/Falafel_Baribua_Embed.png');
 
